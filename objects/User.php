25a9073d6bf097feb41a1c3a5a40c8cb1ff4681c
Thomas Olsson <olss.thom@gmail.com>
2021-03-25 01:21:03 +0100

    hash password at register & dehash at login

--- objects/User.php
+++ objects/User.php
@@ -22,24 +22,25 @@ class User{
     public function register($email_IN, $username_IN, $password_IN){   
         $sql = "INSERT INTO users (email, username, password) VALUES (:email_IN, :username_IN, :password_IN)";
 
+        $hashedPassword = password_hash($password_IN, PASSWORD_DEFAULT);
+
         $stmt = $this->dbConnect->prepare($sql);
         $stmt->bindParam(":email_IN", $email_IN);
         $stmt->bindParam(":username_IN", $username_IN);
-        $stmt->bindParam(":password_IN", $password_IN);
+        $stmt->bindParam(":password_IN", $hashedPassword);
 
         return $stmt->execute();
     }
 
-    public function login($username_IN, $password_IN){
-        $sql = "SELECT * FROM users WHERE username = :username_IN AND password = :password_IN";
+    public function login($username_IN, $password_verify_IN){
+        $sql = "SELECT * FROM users WHERE username = :username_IN";
 
         $stmt = $this->dbConnect->prepare($sql);
         $stmt->bindParam(":username_IN", $username_IN);
-        $stmt->bindParam(":password_IN", $password_IN);
-        
         $stmt->execute();
 
-        if ($stmt->rowCount() == 1){
+        //Checks if input matches a record in database & verifies hashed password
+        if ($stmt->rowCount() == 1 && $password_verify_IN == 1){
             echo json_encode("User has successfully been logged in");
         } else {
             echo json_encode("Invalid login credentials");
--- v1/users/login.php
+++ v1/users/login.php
@@ -4,13 +4,23 @@ header('Content-Type: application/json');
 include("../../config/db.php");
 include("../../objects/User.php");
 
+
+
 if (isset($_GET['username']) && isset($_GET['password'])){
-    $username = $_GET['username'];
-    $password = $_GET['password'];
+
+    //Verify hashed password: returns true if correct password
+    function pwdVerify($db){
+        $sql = "SELECT password FROM users WHERE username = :username_IN";
+        $stm = $db->prepare($sql);
+        $stm->bindParam(":username_IN", $_GET['username']);
+        $stm->execute();
+        $hashed_pwd = $stm->fetch()['password'];
+        $pwd_verify = password_verify($_GET['password'], $hashed_pwd);
+        return $pwd_verify;
+    }
 
     $user = new User($db);
-
-    $user->login($username, $password);
+    $user->login($_GET['username'], pwdVerify($db));
 
 } else {
     echo json_encode("Error: username and password needs to be specified");
