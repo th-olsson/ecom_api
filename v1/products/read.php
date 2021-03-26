181eaa35c5645a16d8b2cb26a070101a22963424
Thomas Olsson <olss.thom@gmail.com>
2021-03-26 21:01:40 +0100

    test demonstrate token auth works on products read

--- v1/products/read.php
+++ v1/products/read.php
@@ -3,8 +3,24 @@ header('Content-Type: application/json');
 
 include("../../config/db.php");
 include("../../objects/Product.php");
+include("../../objects/User.php");
+$user = new User($db);
+
+$token="";
+if (isset($_GET['token'])){
+    $token = $_GET['token'];
+} else {
+    echo json_encode("No token specified");
+    die();
+}
+
 
 $product = new Product($db);
 
-$product->read();
+if ($user->isTokenValid($token)){
+    $product->read();
+} else {
+    echo json_encode("You need a valid token to view this page");
+}
+
 ?>
