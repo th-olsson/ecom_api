6eb7aa6bbeedd99580100ee183292d17b402e010
Thomas Olsson <olss.thom@gmail.com>
2021-03-31 18:31:04 +0200

    gives db-variables more unique names

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php #Connects to database using PDO.
 
-$host = 'localhost';
-$dbname = 'ecom_api';
-$username = 'root';
-$password = '';
+$db_host = 'localhost';
+$db_dbname = 'ecom_api';
+$db_username = 'root';
+$db_password = '';
 
-$db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$db = new PDO("mysql:host=$db_host;dbname=$db_dbname", $db_username, $db_password);
 ?>
