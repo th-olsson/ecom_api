927f9bb4c5f4e68aceeac41ae309305847d7e6d7
Thomas Olsson <olss.thom@gmail.com>
2021-04-02 18:26:44 +0200

    removes token required to read products

--- v1/products/read.php
+++ v1/products/read.php
@@ -3,24 +3,8 @@ header('Content-Type: application/json');
 
 include("../../config/db.php");
 include("../../objects/Product.php");
-include("../../objects/User.php");
-$user = new User($db);
-
-$token="";
-if (isset($_GET['token'])){
-    $token = $_GET['token'];
-} else {
-    echo json_encode("No token specified");
-    die();
-}
-
 
 $product = new Product($db);
 
-if ($user->isTokenValid($token)){
-    $product->read();
-} else {
-    echo json_encode("You need a valid token to view this page");
-}
-
+$product->read();
 ?>
